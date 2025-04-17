300ce40aca3d34630279deaebfbdd1285f0988c3
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-04-16 20:27:39 -0400

    feat(PRICING): add pricing model and serializers

---
+++ cross_shopper/reports/models/serializers/tests/conftest.py
@@ -0,0 +1,16 @@
+"""Test fixtures for the report models serializers."""
+
+from unittest import mock
+
+import pytest
+from pricing.models import Price
+
+
+@pytest.fixture
+def mocked_aggregate_last_52_weeks_manager(
+    monkeypatch: pytest.MonkeyPatch
+) -> mock.Mock:
+  manager_mock = mock.Mock()
+  monkeypatch.setattr(Price, "aggregate_last_52_weeks", manager_mock)
+
+  return manager_mock

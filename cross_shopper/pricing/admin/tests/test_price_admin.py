af8909899a7a33096195fc0ddbe8b240e01d64aa
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-04-16 20:27:39 -0400

    feat(PRICING): add pricing model and serializers

---
+++ cross_shopper/pricing/admin/tests/test_price_admin.py
@@ -0,0 +1,45 @@
+"""Test the admin for the Price model."""
+
+from pricing.admin.price import PriceAdmin
+
+
+class TestPriceAdmin:
+
+  def test_instantiate__has_correct_fieldsets(
+      self,
+      price_admin: PriceAdmin,
+  ) -> None:
+    assert price_admin.fieldsets == (
+        (
+            'Pricing',
+            {
+                'fields': ('amount', 'item', 'store')
+            },
+        ),
+        (
+            'Date',
+            {
+                'fields': ('week', 'year'),
+            },
+        ),
+        (
+            'Historical Data', {
+                'fields':
+                    (
+                        'last_52_weeks_average',
+                        'last_52_weeks_high',
+                        'last_52_weeks_low',
+                    ),
+            }
+        ),
+    )
+
+  def test_instantiate__has_correct_readonly_fields(
+      self,
+      price_admin: PriceAdmin,
+  ) -> None:
+    assert price_admin.readonly_fields == (
+        'last_52_weeks_average',
+        'last_52_weeks_high',
+        'last_52_weeks_low',
+    )

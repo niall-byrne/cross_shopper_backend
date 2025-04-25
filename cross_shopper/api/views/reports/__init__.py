410c33500344cd9a9dbd1acb3cae15f7272d13c9
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-04-25 14:34:04 -0400

    feat(REPORTS): control testing report visibility

--- cross_shopper/api/views/reports/__init__.py
+++ cross_shopper/api/views/reports/__init__.py
@@ -1,5 +1,7 @@
 """Reports API endpoints."""
 
+from django.conf import settings
+from django.db import models
 from reports.models import Report
 from reports.models.serializers.report import ReportSerializer
 from rest_framework import viewsets
@@ -14,3 +16,12 @@ class ReportsReadOnlyViewSet(
   queryset = Report.objects.all()
   serializer_class = ReportSerializer
   filterset_class = ReportFilter
+
+  def get_queryset(self) -> "models.QuerySet[Report]":
+    """Retrieve the list of reports for this view."""
+    is_testing = getattr(
+        settings,
+        "QUERY_PARAMETER_REPORT_TESTING",
+        None,
+    ) in self.request.query_params
+    return self.queryset.filter(is_testing_only=is_testing)

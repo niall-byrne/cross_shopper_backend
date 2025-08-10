665b1320a8734135a66099a2a1085f6bbfef0b3d
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-08-10 16:37:02 -0400

    feat(ADMIN): add search fields to key models

--- cross_shopper/reports/admin/tests/test_report_store_admin.py
+++ cross_shopper/reports/admin/tests/test_report_store_admin.py
@@ -10,3 +10,12 @@ class TestReportStoreAdmin:
       report_store_admin: ReportStoreAdmin,
   ) -> None:
     assert report_store_admin.ordering == ("store__franchise__name", "store")
+
+  def test_instantiate__has_correct_search_fields(
+      self,
+      report_store_admin: ReportStoreAdmin,
+  ) -> None:
+    assert report_store_admin.search_fields == (
+        "store__address__locality__name",
+        "store__franchise__name",
+    )

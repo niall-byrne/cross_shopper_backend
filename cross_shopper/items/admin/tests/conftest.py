b2fa226d3912be3a31ebd3a91c9419608c4aafb3
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-10-03 12:21:11 -0400

    feat(ADMIN): sort packaging containers and units

--- cross_shopper/items/admin/tests/conftest.py
+++ cross_shopper/items/admin/tests/conftest.py
@@ -4,8 +4,7 @@ from unittest import mock
 
 import pytest
 from django.contrib import admin
-from items.admin import item
-from items.admin.item import ItemAdmin
+from items.admin import item, packaging_container, packaging_unit
 
 
 @pytest.fixture
@@ -58,7 +57,7 @@ def item_admin(
     mocked_model_manager: mock.Mock,
     mocked_report_manger: mock.Mock,
     monkeypatch: pytest.MonkeyPatch,
-) -> ItemAdmin:
+) -> item.ItemAdmin:
   monkeypatch.setattr(
       admin.ModelAdmin,
       "formfield_for_foreignkey",
@@ -80,7 +79,31 @@ def item_admin(
       mocked_model_manager,
   )
 
-  return ItemAdmin(
+  return item.ItemAdmin(
+      model=mocked_model,
+      admin_site=mocked_admin_site,
+  )
+
+
+@pytest.fixture
+def packaging_container_admin(
+    mocked_admin_site: mock.Mock,
+    mocked_model: mock.Mock,
+) -> packaging_container.PackagingContainerAdmin:
+
+  return packaging_container.PackagingContainerAdmin(
+      model=mocked_model,
+      admin_site=mocked_admin_site,
+  )
+
+
+@pytest.fixture
+def packaging_unit_admin(
+    mocked_admin_site: mock.Mock,
+    mocked_model: mock.Mock,
+) -> packaging_unit.PackagingUnitAdmin:
+
+  return packaging_unit.PackagingUnitAdmin(
       model=mocked_model,
       admin_site=mocked_admin_site,
   )

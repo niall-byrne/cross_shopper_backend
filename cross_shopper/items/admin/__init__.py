48f528786fefd2003e7f3cf7138891cfd663d796
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2025-10-03 12:21:11 -0400

    feat(ADMIN): sort packaging containers and units

--- cross_shopper/items/admin/__init__.py
+++ cross_shopper/items/admin/__init__.py
@@ -2,6 +2,8 @@
 
 from django.contrib import admin
 from items.admin.item import ItemAdmin
+from items.admin.packaging_container import PackagingContainerAdmin
+from items.admin.packaging_unit import PackagingUnitAdmin
 from items.models import (
     Brand,
     Item,
@@ -15,5 +17,5 @@ admin.site.register(Brand)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(ItemScraperConfig)
 admin.site.register(Packaging)
-admin.site.register(PackagingContainer)
-admin.site.register(PackagingUnit)
+admin.site.register(PackagingContainer, PackagingContainerAdmin)
+admin.site.register(PackagingUnit, PackagingUnitAdmin)

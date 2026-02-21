a51f4b0bcebb0df275be955e62d2fdb7072bee5c
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-21 16:36:05 -0500

    feat(SCRAPERS): enhance scraper config model admins

---
+++ cross_shopper/scrapers/admin/mixins/types.py
@@ -0,0 +1,22 @@
+"""Type for the scrapers app admin model mixin classes."""
+
+from typing import TYPE_CHECKING, Protocol, TypeVar, Union
+
+if TYPE_CHECKING:  # no cover
+  from django.db.models import Model
+  from django.http import HttpRequest
+
+ModelType = TypeVar('ModelType', bound='Model')
+
+
+class AdminMixinType(Protocol):
+
+  def message_user( # noqa: D102
+      self,
+      request: "HttpRequest",
+      message: str,
+      level: Union[int, str] = ...,
+      extra_tags: str = ...,
+      fail_silently: bool = ...,
+  ) -> None:
+    ...

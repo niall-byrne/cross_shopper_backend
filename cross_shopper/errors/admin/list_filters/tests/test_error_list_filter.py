1948b28ab2e1076a11837d6bd3e0b2803854e67d
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-20 21:13:50 -0500

    feat(ERRORS): track scraping errors

---
+++ cross_shopper/errors/admin/list_filters/tests/test_error_list_filter.py
@@ -0,0 +1,48 @@
+"""Test the Error admin model list filter."""
+
+from errors.admin.list_filters.error import error_list_filter
+from utilities.admin.list_filters import GenericListFilter
+from utilities.testing.comparisons.is_subclass import DynamicSubClass
+
+
+class TestErrorAdminListFilter:
+
+  def test_list_filter(self) -> None:
+    assert error_list_filter == (
+        DynamicSubClass(
+            base=GenericListFilter,
+            attributes={
+                "title": "type",
+                "parameter_name": "type__name",
+                "is_boolean": False,
+                "is_reversed": False,
+            },
+        ),
+        DynamicSubClass(
+            base=GenericListFilter,
+            attributes={
+                "title": "franchise",
+                "parameter_name": "store__franchise__name",
+                "is_boolean": False,
+                "is_reversed": False,
+            },
+        ),
+        DynamicSubClass(
+            base=GenericListFilter,
+            attributes={
+                "title": "item",
+                "parameter_name": "item__name",
+                "is_boolean": False,
+                "is_reversed": False,
+            },
+        ),
+        DynamicSubClass(
+            base=GenericListFilter,
+            attributes={
+                "title": "scraper",
+                "parameter_name": "scraper_config__scraper__name",
+                "is_boolean": False,
+                "is_reversed": False,
+            },
+        ),
+    )
---
+++ cross_shopper/errors/models/error.py
@@ -0,0 +1,32 @@
+"""Error model."""
+
+from django.db import models
+from utilities.models.bases.model_base import ModelBase
+
+
+class Error(
+    ModelBase,
+):
+  is_reoccurring = models.BooleanField(default=False)
+
+  type = models.ForeignKey("errors.ErrorType", on_delete=models.CASCADE)
+  item = models.ForeignKey("items.Item", on_delete=models.CASCADE)
+  scraper_config = models.ForeignKey(
+      "scrapers.ScraperConfig",
+      on_delete=models.CASCADE,
+  )
+  store = models.ForeignKey("stores.Store", on_delete=models.CASCADE)
+  count = models.PositiveIntegerField(default=1)
+
+  class Meta:
+    unique_together = ("type", "item", "scraper_config", "store")
+
+  def __str__(self) -> str:
+    return " ".join(
+        [
+            str(self.type) + ":",
+            str(self.item.full_name),
+            "-",
+            str(self.store),
+        ]
+    )

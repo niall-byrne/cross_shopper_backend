0375571b4491a68f6b0e1283a48dea01f9c02492
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-20 21:13:50 -0500

    feat(ERRORS): track scraping errors

---
+++ cross_shopper/api/views/errors/tests/test_error_update.py
@@ -0,0 +1,67 @@
+"""Test the ErrorsViewSet create view."""
+
+from typing import TYPE_CHECKING
+
+import pytest
+from errors.models.serializers.error import ErrorSerializer
+from rest_framework import status
+
+if TYPE_CHECKING:  # nocover
+  from errors.models import Error
+  from rest_framework.test import APIClient
+  from .conftest import AliasErrorDetailUrl
+
+
+@pytest.mark.django_db
+class TestErrorsViewSetUpdateNoAuthentication:
+
+  def test_update__forbids_access(
+      self,
+      unauthenticated_client: "APIClient",
+      error_detail_url: "AliasErrorDetailUrl",
+      error: "Error",
+  ) -> None:
+    serializer = ErrorSerializer(error)
+    data = dict(serializer.data)
+
+    res = unauthenticated_client.put(error_detail_url(error.pk), data=data)
+
+    assert res.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+class TestErrorsViewSetUpdateAuthentication:
+
+  def test_update__existing_object__returns_correct_response(
+      self,
+      authenticated_client: "APIClient",
+      error_detail_url: "AliasErrorDetailUrl",
+      error: "Error",
+  ) -> None:
+    serializer = ErrorSerializer(error)
+    update_data = dict(serializer.data)
+
+    res = authenticated_client.put(
+        error_detail_url(error.pk),
+        data=update_data,
+    )
+
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data == update_data
+
+  def test_update__existing_object__does_not_increment_error_count(
+      self,
+      authenticated_client: "APIClient",
+      error_detail_url: "AliasErrorDetailUrl",
+      error: "Error",
+  ) -> None:
+    serializer = ErrorSerializer(error)
+    update_data = dict(serializer.data)
+
+    authenticated_client.put(
+        error_detail_url(error.pk),
+        data=update_data,
+    )
+
+    error.refresh_from_db()
+    assert error.count == 1

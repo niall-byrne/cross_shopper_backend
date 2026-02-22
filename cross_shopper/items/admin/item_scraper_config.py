ce52eca65df864b68a87923da775a30a9aa79e67
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-22 15:09:12 -0500

    feat(ITEMS): add columns to item scraper config admin

--- cross_shopper/items/admin/item_scraper_config.py
+++ cross_shopper/items/admin/item_scraper_config.py
@@ -3,17 +3,22 @@
 from typing import TYPE_CHECKING
 
 from django.contrib import admin
+from items.admin.list_display.item_scraper_config import (
+    item_scraper_config_list_display,
+)
 from items.admin.list_filter.item_scraper_config import (
     item_scraper_config_list_filter,
 )
 from scrapers.admin.mixins.scraper_config_actions import (
     ScraperConfigActionsAdminMixin,
 )
+from utilities.admin.list_display import generate_list_display
 
 if TYPE_CHECKING:  # no cover
   from items.models import ItemScraperConfig  # noqa: F401
 
 
+@generate_list_display(item_scraper_config_list_display)
 class ItemScraperConfigAdmin(
     ScraperConfigActionsAdminMixin["ItemScraperConfig"],
     admin.ModelAdmin["ItemScraperConfig"],

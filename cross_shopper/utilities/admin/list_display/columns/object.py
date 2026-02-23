7a0f5f0ce268bc27a8dcd90d3921b6f23b372a9d
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-23 09:18:45 -0500

    feat(UTILITIES): admin list view column generator

---
+++ cross_shopper/utilities/admin/list_display/columns/object.py
@@ -0,0 +1,34 @@
+"""Configuration for a list display column containing a model."""
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from utilities.admin.list_display.columns.bases.column_base import (
+    ColumnConfigBase,
+)
+
+if TYPE_CHECKING:  # no cover
+  from typing import Optional
+
+  from utilities.admin.list_display.columns.typing import AliasColumnMethod
+
+
+@dataclass
+class ColumnObjectConfig(ColumnConfigBase):
+  """Configuration for a list display column containing a model."""
+
+  method_name: str
+  description: str
+  obj_lookup: str
+  obj_order: "Optional[str]" = ""
+  is_boolean: bool = False
+
+  def get_method(self) -> "AliasColumnMethod":
+    """Return the method used to generate the column data."""
+    return lambda _, obj: self._get_attr_with_default(obj, self.obj_lookup)
+
+  def get_ordering(self) -> "Optional[str]":
+    """Return the lookup used for column ordering."""
+    if self.obj_order == "":
+      return self.obj_lookup
+    return self.obj_order
---
+++ cross_shopper/utilities/admin/list_display/tests/conftest.py
@@ -0,0 +1,63 @@
+"""Test fixtures for the utilities app model admins."""
+
+from typing import Tuple
+from unittest import mock
+
+import pytest
+from utilities.admin.list_display.columns import (
+    ColumnLinkConfig,
+    ColumnObjectConfig,
+)
+
+
+@pytest.fixture
+def column_config_1() -> Tuple[ColumnLinkConfig]:
+  return (
+      ColumnLinkConfig(
+          method_name="method_name",
+          description="simple description",
+          reverse_url_name="admin:stores_store_change",
+          obj_id_lookup="related.field.id",
+          obj_name_lookup="related.field.name",
+      ),
+  )
+
+
+@pytest.fixture
+def column_config_2() -> Tuple[ColumnLinkConfig, ColumnObjectConfig]:
+  return (
+      ColumnLinkConfig(
+          method_name="method_name_1",
+          description="simple description 1",
+          reverse_url_name="admin:stores_store_change",
+          obj_id_lookup="related.field1.id",
+          obj_name_lookup="related.field1.name",
+      ),
+      ColumnObjectConfig(
+          method_name="method_name_2",
+          description="simple description 2",
+          obj_lookup=""
+      ),
+  )
+
+
+@pytest.fixture
+def column_config_3() -> Tuple[ColumnObjectConfig]:
+  return (
+      ColumnObjectConfig(
+          method_name="boolean_method_name",
+          description="boolean simple description",
+          obj_lookup="boolean.related.field",
+          is_boolean=True,
+      ),
+  )
+
+
+@pytest.fixture
+def mocked_admin() -> mock.Mock:
+  return mock.Mock()
+
+
+@pytest.fixture
+def mocked_model() -> mock.Mock:
+  return mock.Mock()

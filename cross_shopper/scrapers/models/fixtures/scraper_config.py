874f267092421033062fc9daf44f199fbbeb6789
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-23 13:08:12 -0500

    feat(ITEMS): model manager for item scraper config

--- cross_shopper/scrapers/models/fixtures/scraper_config.py
+++ cross_shopper/scrapers/models/fixtures/scraper_config.py
@@ -3,12 +3,21 @@
 from typing import TYPE_CHECKING
 
 import pytest
+from scrapers.models import ScraperConfig
 from scrapers.models.factories.scraper_config import ScraperConfigFactory
 
 if TYPE_CHECKING:  # no cover
-  from scrapers.models import ScraperConfig
+  from django.db.models import QuerySet
 
 
 @pytest.fixture
 def scraper_config() -> "ScraperConfig":
   return ScraperConfigFactory.create()
+
+
+@pytest.fixture
+def scraper_config_batch() -> "QuerySet[ScraperConfig]":
+  item_scraper_configs = ScraperConfigFactory.create_batch(size=10)
+  return ScraperConfig.objects.filter(
+      id__in=[instance.id for instance in item_scraper_configs]
+  )

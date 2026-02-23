91160fb9085f488434d6eb769acbdfbb7b75f116
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-23 08:34:34 -0500

    feat(ITEMS): constrain item scraper configs

--- cross_shopper/items/models/tests/test_item_scraper_config.py
+++ cross_shopper/items/models/tests/test_item_scraper_config.py
@@ -4,13 +4,65 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
+from django.core.exceptions import ValidationError
+from items.models import ItemScraperConfig
 
 if TYPE_CHECKING:
-  from items.models import ItemScraperConfig
+  from items.models import Item
 
 
 @pytest.mark.django_db
 class TestItemScraperConfig:
+
+  def test_constraint__unique_together(
+      self,
+      item_scraper_config: ItemScraperConfig,
+  ) -> None:
+    item_scraper_config2 = ItemScraperConfig(
+        item=item_scraper_config.item,
+        scraper_config=item_scraper_config.scraper_config,
+    )
+
+    with pytest.raises(ValidationError) as exc:
+      item_scraper_config2.save()
+
+    assert str(exc.value) == str(
+        {
+            "__all__":
+                [
+                    "Item scraper config with this Item and Scraper config "
+                    "already exists."
+                ],
+            "scraper_config":
+                [
+                    "Item scraper config with this Scraper config already "
+                    "exists."
+                ]
+        }
+    )
+
+  def test_constraint__unique_scraper_config(
+      self,
+      item: Item,
+      item_scraper_config: ItemScraperConfig,
+  ) -> None:
+    item_scraper_config2 = ItemScraperConfig(
+        item=item,
+        scraper_config=item_scraper_config.scraper_config,
+    )
+
+    with pytest.raises(ValidationError) as exc:
+      item_scraper_config2.save()
+
+    assert str(exc.value) == str(
+        {
+            "scraper_config":
+                [
+                    "Item scraper config with this Scraper config already "
+                    "exists."
+                ]
+        }
+    )
 
   def test_str__returns_str_of_scraper_config(
       self,

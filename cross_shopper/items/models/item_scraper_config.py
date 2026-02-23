dd858ff913ed1714ae6ef76f78d5e4ddb2d25f32
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-23 08:34:34 -0500

    feat(ITEMS): constrain item scraper configs

--- cross_shopper/items/models/item_scraper_config.py
+++ cross_shopper/items/models/item_scraper_config.py
@@ -1,6 +1,7 @@
 """ItemScraperConfig model."""
 
 from django.db import models
+from django.db.models import UniqueConstraint
 from utilities.models.bases.base_model import BaseModel
 
 
@@ -15,6 +16,15 @@ class ItemScraperConfig(
       'scrapers.ScraperConfig',
       on_delete=models.PROTECT,
   )
+
+  class Meta:
+    unique_together = ('item', 'scraper_config')
+    constraints = [
+        UniqueConstraint(
+            fields=['scraper_config'],
+            name='unique_scraper_config',
+        )
+    ]
 
   def __str__(self) -> str:
     return f"{self.item.name} - {self.scraper_config}"

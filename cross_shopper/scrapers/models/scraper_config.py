3f721a907897f7dc8b0837eb430cebea6cdad090
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-25 18:16:18 -0500

    feat(SCRAPERS): add related item properties to scraper configs

--- cross_shopper/scrapers/models/scraper_config.py
+++ cross_shopper/scrapers/models/scraper_config.py
@@ -1,10 +1,13 @@
 """ScraperConfig model."""
 
 import re
+from typing import Optional
 
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.functions import Lower
+from django.utils.functional import cached_property
+from items.models import Item, ItemScraperConfig
 from utilities.models.bases.model_base import ModelBase
 
 CONSTRAINT_NAMES = {"url": "URL name must be unique"}
@@ -32,6 +35,16 @@ class ScraperConfig(
       blank=False,
   )
   is_active = models.BooleanField(default=True)
+
+  @property
+  def has_item(self) -> bool:
+    """Return a boolean if an item is associated with this instance."""
+    return self.associated_item is not None
+
+  @cached_property
+  def associated_item(self) -> Optional[Item]:
+    """Return the item associated with this instance, if one exists."""
+    return ItemScraperConfig.associations.get_item(self)
 
   def clean(self) -> None:
     """Pre-save verification."""

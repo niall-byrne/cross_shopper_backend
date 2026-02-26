600cb37538d9698e2e43ed1f57dc0f6fc5519a25
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-26 11:46:52 -0500

    feat(REPORTS): filter scraper configs on is active

--- cross_shopper/reports/models/serializers/tests/conftest.py
+++ cross_shopper/reports/models/serializers/tests/conftest.py
@@ -1,9 +1,25 @@
 """Test fixtures for the report models serializers."""
 
+from typing import Callable, Dict
 from unittest import mock
 
 import pytest
+from django.http import HttpRequest
 from pricing.models import Price
+from rest_framework import request
+
+AliasCreateMockedRequest = Callable[[Dict[str, str]], request.Request]
+
+
+@pytest.fixture
+def create_mocked_request() -> AliasCreateMockedRequest:
+
+  def create(query_param: Dict[str, str]) -> request.Request:
+    http_request = HttpRequest()
+    http_request.GET.update(query_param)
+    return request.Request(http_request)
+
+  return create
 
 
 @pytest.fixture

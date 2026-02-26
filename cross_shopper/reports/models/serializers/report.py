d5772fe9703bfc0cfa9b8cec2d17fc5064748efb
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-26 11:46:52 -0500

    feat(REPORTS): filter scraper configs on is active

--- cross_shopper/reports/models/serializers/report.py
+++ cross_shopper/reports/models/serializers/report.py
@@ -1,7 +1,5 @@
 """Serializer for the Report model."""
 
-from typing import Any, Dict
-
 from items.models.serializers.item import ItemSerializer
 from reports.models import Report
 from rest_framework import serializers
@@ -14,23 +12,10 @@ class ReportSerializer(serializers.ModelSerializer[Report]):
 
   name = TitleField(max_length=80, allow_blank=False)
   user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-  item = serializers.SerializerMethodField()
+  item = ItemSerializer(many=True)
   store = StoreSerializer(many=True)
   is_testing_only = serializers.BooleanField(default=False)
 
   class Meta:
     model = Report
     fields = ('id', 'name', 'item', 'store', 'user', 'is_testing_only')
-
-  ITEM_FIELD_ORDERING = (
-      'name',
-      'brand__name',
-      'is_organic',
-      'packaging__container',
-      'packaging__quantity',
-  )
-
-  def get_item(self, instance: Report) -> Dict[str, Any]:
-    """Get the serialized item model representation."""
-    item = instance.item.all().order_by(*self.ITEM_FIELD_ORDERING)
-    return ItemSerializer(item, many=True).data

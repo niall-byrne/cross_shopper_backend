2eac8d843ba1dc6029f9bd408b14d40d7b77d877
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-26 11:46:52 -0500

    feat(REPORTS): filter scraper configs on is active

--- cross_shopper/api/views/reports/__init__.py
+++ cross_shopper/api/views/reports/__init__.py
@@ -1,9 +1,16 @@
 """Reports API endpoints."""
 
+from typing import TYPE_CHECKING
+
+from django.db.models import Prefetch
 from reports.models import Report
 from reports.models.serializers.report import ReportSerializer
 from rest_framework import viewsets
 from .filters import ReportFilter
+from .qs import qs_item
+
+if TYPE_CHECKING:  # no cover
+  from django.db.models import QuerySet
 
 
 class ReportsReadOnlyViewSet(
@@ -14,3 +21,12 @@ class ReportsReadOnlyViewSet(
   queryset = Report.objects.all()
   serializer_class = ReportSerializer
   filterset_class = ReportFilter
+
+  def get_queryset(self) -> "QuerySet[Report]":
+    """Retrieve the list of reports for this view."""
+    return self.queryset.prefetch_related(
+        Prefetch(
+            'item',
+            queryset=qs_item(),
+        )
+    )

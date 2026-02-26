ec82be528efe0df72969c260015d993669dd32f6
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-26 11:46:52 -0500

    feat(REPORTS): filter scraper configs on is active

--- cross_shopper/api/views/reports/filters.py
+++ cross_shopper/api/views/reports/filters.py
@@ -1,14 +1,50 @@
 """Reports API endpoints filters."""
 
+from typing import TYPE_CHECKING
+
 import django_filters
+from django.db.models import Prefetch
 from reports.models import Report
+from .qs import qs_item, qs_scraper_config
+
+if TYPE_CHECKING:  # no cover
+  from django.db.models import QuerySet
 
 
 class ReportFilter(django_filters.FilterSet):
   """Reports API endpoint filter."""
 
   name = django_filters.CharFilter(field_name="name", lookup_expr="iexact")
+  is_active = django_filters.BooleanFilter(
+      field_name="is_active",
+      method="get_query_set__is_active",
+  )
 
   class Meta:
     model = Report
-    fields = ["id", "name"]
+    fields = ["id", "name", "is_active"]
+
+  def get_query_set__is_active(
+      self,
+      queryset: "QuerySet[Report]",
+      field_name: str,
+      value: bool,
+  ) -> "QuerySet[Report]":
+    """Filter each Item's ScraperConfigs in the Report query set."""
+    prefetched_scraper_config = qs_scraper_config().filter(
+        **{field_name: value}
+    )
+
+    prefetched_item = qs_item().prefetch_related(
+        Prefetch(
+            "scraper_config",
+            queryset=prefetched_scraper_config,
+        )
+    )
+
+    return queryset.prefetch_related(None).prefetch_related(
+        Prefetch(
+            "item",
+            queryset=prefetched_item,
+        )
+    )
--- cross_shopper/reports/models/serializers/tests/test_report.py
+++ cross_shopper/reports/models/serializers/tests/test_report.py
@@ -9,6 +9,7 @@ from stores.models.serializers.store import StoreSerializer
 
 if TYPE_CHECKING:  # no cover
   from reports.models import Report
+  from .conftest import AliasCreateMockedRequest
 
 
 @pytest.mark.django_db
@@ -17,26 +18,22 @@ class TestReportSerializer:
   def test_serialization__correct_representation(
       self,
       report: "Report",
+      create_mocked_request: "AliasCreateMockedRequest",
   ) -> None:
-    serialized = ReportSerializer(report, context={})
+    serialized = ReportSerializer(
+        report, context={"request": create_mocked_request({})}
+    )
 
     assert serialized.data == {
-        "id":
-            report.pk,
-        "name":
-            report.name,
-        "item":
-            ItemSerializer(
-                report.item.all().order_by(
-                    *ReportSerializer.ITEM_FIELD_ORDERING
-                ),
-                many=True,
-            ).data,
-        "store":
-            StoreSerializer(
-                report.store,
-                many=True,
-            ).data,
-        "is_testing_only":
-            report.is_testing_only,
+        "id": report.pk,
+        "name": report.name,
+        "item": ItemSerializer(
+            report.item.all(),
+            many=True,
+        ).data,
+        "store": StoreSerializer(
+            report.store,
+            many=True,
+        ).data,
+        "is_testing_only": report.is_testing_only,
     }

e3061e5f2f654204af319300c02bdb2aa54bd74a
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-27 20:16:45 -0500

    feat(REPORTS): add columns to report store admin

---
+++ cross_shopper/reports/admin/list_display/report_store.py
@@ -0,0 +1,35 @@
+"""ReportStore admin model list display."""
+
+from utilities.admin.list_display.columns import (
+    ColumnLinkConfig,
+    ColumnObjectConfig,
+)
+
+report_store_list_display = (
+    ColumnObjectConfig(
+        method_name="report_store",
+        description="Report Store",
+        obj_lookup="",
+    ),
+    ColumnLinkConfig(
+        method_name="store__report",
+        description="Report",
+        reverse_url_name="admin:reports_report_change",
+        obj_id_lookup="report.id",
+        obj_name_lookup="report.name",
+    ),
+    ColumnLinkConfig(
+        method_name="store__address",
+        description="Store",
+        reverse_url_name="admin:stores_store_change",
+        obj_id_lookup="store.id",
+        obj_name_lookup="store.address",
+    ),
+    ColumnLinkConfig(
+        method_name="store__franchise",
+        description="Franchise",
+        reverse_url_name="admin:stores_franchise_change",
+        obj_id_lookup="store.franchise.id",
+        obj_name_lookup="store.franchise.name",
+    ),
+)
---
+++ cross_shopper/reports/admin/list_display/tests/test_report_store_list_display.py
@@ -0,0 +1,55 @@
+"""Test the ReportStore admin model list display."""
+
+from reports.admin.list_display.report_store import (
+    report_store_list_display,
+)
+from utilities.admin.list_display.columns import (
+    ColumnLinkConfig,
+    ColumnObjectConfig,
+)
+from utilities.testing.comparisons.instance import InstanceOfClass
+
+
+class TestReportStoreAdminListDisplay:
+
+  def test_list_display(self) -> None:
+    assert report_store_list_display == (
+        InstanceOfClass(
+            base=ColumnObjectConfig,
+            attributes={
+                "method_name": "report_store",
+                "description": "Report Store",
+                "obj_lookup": "",
+            }
+        ),
+        InstanceOfClass(
+            base=ColumnLinkConfig,
+            attributes={
+                "method_name": "store__report",
+                "description": "Report",
+                "reverse_url_name": "admin:reports_report_change",
+                "obj_id_lookup": "report.id",
+                "obj_name_lookup": "report.name",
+            }
+        ),
+        InstanceOfClass(
+            base=ColumnLinkConfig,
+            attributes={
+                "method_name": "store__address",
+                "description": "Store",
+                "reverse_url_name": "admin:stores_store_change",
+                "obj_id_lookup": "store.id",
+                "obj_name_lookup": "store.address",
+            }
+        ),
+        InstanceOfClass(
+            base=ColumnLinkConfig,
+            attributes={
+                "method_name": "store__franchise",
+                "description": "Franchise",
+                "reverse_url_name": "admin:stores_franchise_change",
+                "obj_id_lookup": "store.franchise.id",
+                "obj_name_lookup": "store.franchise.name",
+            }
+        ),
+    )

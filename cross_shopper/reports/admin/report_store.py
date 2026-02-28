0962ae8d0270ee3baee30b025a6e07ea3bebedab
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-02-27 20:16:45 -0500

    feat(REPORTS): add columns to report store admin

--- cross_shopper/reports/admin/report_store.py
+++ cross_shopper/reports/admin/report_store.py
@@ -1,10 +1,13 @@
 """Admin models for the Report model."""
 
 from django.contrib import admin
+from reports.admin.list_displays.report_store import report_store_list_display
 from reports.admin.list_filters.report_store import report_store_list_filter
 from reports.models import Report
+from utilities.admin.list_displays.decorator import generate_list_display
 
 
+@generate_list_display(report_store_list_display)
 class ReportStoreAdmin(admin.ModelAdmin[Report]):
   list_filter = report_store_list_filter
   ordering = ("store__franchise__name", "store")

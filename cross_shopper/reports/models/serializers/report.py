b40b31ac56c19a9ceac8dbb681b6d5fd9508a0ab
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-02 14:46:06 -0500

    refactor(STORES): standardize stores

--- cross_shopper/reports/models/serializers/report.py
+++ cross_shopper/reports/models/serializers/report.py
@@ -3,7 +3,7 @@
 from items.models.serializers.item import ItemSerializer
 from reports.models import Report
 from rest_framework import serializers
-from stores.models.serializers.store import StoreSerializer
+from stores.models.serializers.read_only.store import StoreSerializerRO
 from utilities.models.serializers.fields.title import TitleField
 
 
@@ -13,7 +13,7 @@ class ReportSerializer(serializers.ModelSerializer[Report]):
   name = TitleField(max_length=80, allow_blank=False)
   user = serializers.HiddenField(default=serializers.CurrentUserDefault())
   item = ItemSerializer(many=True)
-  store = StoreSerializer(many=True)
+  store = StoreSerializerRO(many=True)
   is_testing = serializers.BooleanField(default=False)
 
   class Meta:
---
+++ cross_shopper/stores/models/serializers/read_write/address/state.py
@@ -0,0 +1,15 @@
+"""Serializer to retrieve, list, create or update States."""
+
+from address.models import State
+from rest_framework import serializers
+from .country import CountrySerializer
+
+
+class StateSerializer(serializers.ModelSerializer[State]):
+  """Serializer to retrieve, list, create or update States."""
+
+  country = CountrySerializer()
+
+  class Meta:
+    model = State
+    fields = '__all__'

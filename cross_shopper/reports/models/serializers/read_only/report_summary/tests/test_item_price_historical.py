3be3fc968eb40ec36ce733bf86e286b1e9d4fe75
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-02 18:36:00 -0500

    refactor(REPORTS): standardize serializers

--- cross_shopper/reports/models/serializers/read_only/report_summary/tests/test_item_price_historical.py
+++ cross_shopper/reports/models/serializers/read_only/report_summary/tests/test_item_price_historical.py
@@ -1,11 +1,12 @@
-"""Tests for the ReportSummaryHistoricalItemPriceSerializer."""
+"""Tests for the ReportSummaryHistoricalItemPriceSerializerRO."""
 
 from typing import TYPE_CHECKING
 
 import pytest
 from reports.models import Report
-from reports.models.serializers.report_summary.item_price_historical import (
-    ReportSummaryHistoricalItemPriceSerializer,
+from reports.models.serializers.read_only.report_summary.\
+  item_price_historical import (
+    ReportSummaryHistoricalItemPriceSerializerRO,
 )
 
 if TYPE_CHECKING:  # no cover
@@ -13,7 +14,7 @@ if TYPE_CHECKING:  # no cover
 
 
 @pytest.mark.django_db
-class TestReportSummaryHistoricalItemPriceSerializer:
+class TestReportSummaryHistoricalItemPriceSerializerRO:
 
   @pytest.mark.usefixtures(
       "report_summary_mocked_pricing_aggregate_last_52_weeks_manager"
@@ -23,7 +24,7 @@ class TestReportSummaryHistoricalItemPriceSerializer:
       report_prefetched: "Report",
       report_summary_mocked_pricing_aggregate_attributes: "Dict[str, str]",
   ) -> None:
-    serializer = ReportSummaryHistoricalItemPriceSerializer(
+    serializer = ReportSummaryHistoricalItemPriceSerializerRO(
         report_prefetched.item.all()[0],
         context={'report': report_prefetched},
     )
@@ -34,7 +35,7 @@ class TestReportSummaryHistoricalItemPriceSerializer:
       self,
       report_prefetched: "Report",
   ) -> None:
-    serializer = ReportSummaryHistoricalItemPriceSerializer(
+    serializer = ReportSummaryHistoricalItemPriceSerializerRO(
         report_prefetched.item.all()[0],
         context={},
     )
@@ -49,7 +50,7 @@ class TestReportSummaryHistoricalItemPriceSerializer:
       self,
       report_prefetched: "Report",
   ) -> None:
-    serializer = ReportSummaryHistoricalItemPriceSerializer(
+    serializer = ReportSummaryHistoricalItemPriceSerializerRO(
         report_prefetched.item.all()[0],
         context={'report': report_prefetched},
     )
--- cross_shopper/reports/models/serializers/report_summary/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Serializers for Report model summaries."""

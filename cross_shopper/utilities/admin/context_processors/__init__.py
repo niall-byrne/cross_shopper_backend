58f8c1b62fd119da06659be6934cd172e6a72266
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-04 13:17:17 -0500

    feat(DJANGO): discrete production environment

---
+++ cross_shopper/utilities/admin/context_processors/__init__.py
@@ -0,0 +1,14 @@
+"""Django admin template context processors."""
+
+from typing import TYPE_CHECKING
+
+from django.conf import settings
+from django.http import HttpRequest
+
+if TYPE_CHECKING:  # no cover
+  from typing import Any, Dict
+
+
+def base(_request: HttpRequest) -> "Dict[str, Any]":
+  """Create the base admin site context processor."""
+  return {'ENVIRONMENT': settings.ENVIRONMENT}

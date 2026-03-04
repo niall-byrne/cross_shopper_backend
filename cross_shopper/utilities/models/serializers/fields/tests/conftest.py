7420067364a2cdccc35db7dd2d2a5ab129980faa
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-04 10:41:58 -0500

    feat(UTILITIES): custom serializer field from context

--- cross_shopper/utilities/models/serializers/fields/tests/conftest.py
+++ cross_shopper/utilities/models/serializers/fields/tests/conftest.py
@@ -1,10 +1,21 @@
 """Test fixtures for the utilities app serializer fields."""
 
 from typing import Any, Type
+from unittest import mock
 
 import pytest
 from rest_framework import serializers
-from utilities.models.serializers.fields import blonde, lowercase, title
+from utilities.models.serializers.fields import (
+    blonde,
+    context,
+    lowercase,
+    title,
+)
+
+
+@pytest.fixture
+def mocked_model() -> mock.Mock:
+  return mock.Mock()
 
 
 @pytest.fixture
@@ -12,6 +23,19 @@ def blonde_field_serializer() -> Type[serializers.Serializer[Any]]:
 
   class TestSerializer(serializers.Serializer[Any]):
     field = blonde.BlondeCharField(allow_null=True)
+
+  return TestSerializer
+
+
+@pytest.fixture
+def context_field_serializer() -> Type[serializers.Serializer[Any]]:
+
+  class TestSerializer(serializers.Serializer[Any]):
+    field = context.ContextField(context_key="context1")
+    field_with_default = context.ContextField(
+        context_key="context2",
+        default_value=lambda: "default_value",
+    )
 
   return TestSerializer
 

dedb09f9ac9c31fb22b9467e35f4459dfa2cf6b6
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-04 10:44:35 -0500

    feat(REPORTS): report summary representation

---
+++ cross_shopper/reports/models/serializers/report_summary/item.py
@@ -0,0 +1,30 @@
+"""Serializer for Item instances in Report model summaries."""
+
+from items.models import Item
+from items.models.serializers.packaging import PackagingSerializer
+from reports.models.serializers.report_summary.item_price import (
+    ReportSummaryItemPriceSerializer,
+)
+from rest_framework import serializers
+
+
+class ReportSummaryItemSerializer(serializers.ModelSerializer[Item]):
+  """Serializer for Item instances in Report model summaries."""
+
+  brand = serializers.CharField(source='brand.name')
+  packaging = PackagingSerializer()
+  is_bulk = serializers.BooleanField(read_only=True)
+  price = ReportSummaryItemPriceSerializer(source='*', read_only=True)
+
+  class Meta:
+    model = Item
+    fields = (
+        'id',
+        'name',
+        'brand',
+        'is_bulk',
+        'is_organic',
+        'is_non_gmo',
+        'packaging',
+        'price',
+    )

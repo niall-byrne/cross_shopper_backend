312adecd54df9e9d3b0fcc6127e7025992e9773e
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-07 09:33:42 -0500

    refactor(UTILITIES): extract peroxide serializer field mixin

--- cross_shopper/utilities/models/serializers/fields/blonde.py
+++ cross_shopper/utilities/models/serializers/fields/blonde.py
@@ -1,27 +1,19 @@
 """A django-bleach sanitizing CharField implementation."""
 
-from bleach import clean
-from django.conf import settings
-from django_bleach.utils import get_bleach_default_options
 from utilities.models.serializers.fields.bases.transform_base import (
     TransformCharFieldBase,
+)
+from utilities.models.serializers.fields.mixins.peroxide import (
+    PeroxideFieldMixin,
 )
 
 
 class BlondeCharField(
     TransformCharFieldBase,
+    PeroxideFieldMixin,
 ):
   """Adds HTML sanitization & restoration to a CharField."""
 
-  CONFIG_KEY = "BLEACH_RESTORE_CONFIG"
-
   def transform(self, value: str) -> str:
     """Transform the field value into a new form."""
-    bleach_kwargs = get_bleach_default_options()
-
-    clean_value = clean(value, **bleach_kwargs) if value else ""
-
-    for bleached, restored in getattr(settings, self.CONFIG_KEY, {}).items():
-      clean_value = clean_value.replace(bleached, restored)
-
-    return clean_value
+    return self.sanitize(value)

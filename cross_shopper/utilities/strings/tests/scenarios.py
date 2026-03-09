f726e0186b5f659246a1a1ea326b7e5b203a707e
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-08 20:41:45 -0400

    feat(UTILITIES): add title case string transformation

---
+++ cross_shopper/utilities/strings/tests/scenarios.py
@@ -0,0 +1,18 @@
+"""Scenarios for for the utilities app string functions."""
+
+import pytest
+
+title_string_scenarios = pytest.mark.parametrize(
+    "input_string, expected_string", [
+        ("quick brown fox", "Quick Brown Fox"),
+        (" quick brown fox", " Quick Brown Fox"),
+        ("the quick brown fox", "The Quick Brown Fox"),
+        ("jumps over a lazy dog", "Jumps Over a Lazy Dog"),
+        ("an iPhone is a device", "An IPhone Is a Device"),
+        ("...the end", "...The End"),
+        ("isn't it lovely", "Isn't It Lovely"),
+        ("", ""),
+        ("a an the", "A an the"),
+        ("mother-in-law's house", "Mother-In-Law's House"),
+    ]
+)

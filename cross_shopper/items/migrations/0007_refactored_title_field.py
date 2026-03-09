fb4aea7a1bc147cf8c6f1016763c51fb8c2661ad
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-09 09:16:04 -0400

    feat(UTILITIES): migration for title field format

---
+++ cross_shopper/items/migrations/0007_refactored_title_field.py
@@ -0,0 +1,34 @@
+"""PackagingContainer data migration for TitleField refactor."""
+
+from typing import TYPE_CHECKING, cast
+
+from django.db import migrations, transaction
+
+if TYPE_CHECKING:
+  from django.apps.registry import Apps
+  from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+  from items.models import PackagingContainer
+
+
+def save_all_func(
+    apps: "Apps",
+    schema_editor: "BaseDatabaseSchemaEditor",
+) -> None:
+  PackagingContainerModel = cast(
+      "PackagingContainer",
+      apps.get_model("items", "PackagingContainer"),
+  )
+
+  with transaction.atomic():
+    for packaging_container in PackagingContainerModel.objects.all():
+      packaging_container.save()
+
+
+class Migration(migrations.Migration):
+  dependencies = [
+      ("items", "0006_populate_item_attribute"),
+  ]
+
+  operations = [
+      migrations.RunPython(save_all_func, save_all_func),
+  ]

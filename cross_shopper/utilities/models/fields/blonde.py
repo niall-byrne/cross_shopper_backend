f7f888aacff7daf0c2662f6d3c3e89f19677ff97
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-08 20:36:35 -0400

    refactor(UTILITIES): separate transform and bleach field bases

--- cross_shopper/utilities/models/fields/blonde.py
+++ cross_shopper/utilities/models/fields/blonde.py
@@ -1,25 +1,29 @@
 """A django-bleach sanitizing CharField implementation."""
 
-from typing import TypeVar
+from typing import TYPE_CHECKING, TypeVar
 
 from django.conf import settings
 from django_bleach.models import BleachField
-from .bases.transform_base import TransformCharFieldBase
 
 _ST = TypeVar("_ST")
 _GT = TypeVar("_GT")
 
+if TYPE_CHECKING:  # no cover
+  from typing import Any
 
-class BlondeCharField(
-    TransformCharFieldBase[_ST, _GT],
-    BleachField[_ST, _GT],
-):
+  from django.db.models import Model
+
+
+class BlondeCharField(BleachField[_ST, _GT]):
   """Adds HTML sanitization & restoration to a CharField."""
 
   CONFIG_KEY = "BLEACH_RESTORE_CONFIG"
 
-  def transform(self, value: str) -> str:
-    """Transform the field value into a new form."""
-    for bleached, restored in getattr(settings, self.CONFIG_KEY, {}).items():
-      value = value.replace(bleached, restored)
+  def pre_save(self, model_instance: "Model", add: "bool") -> "Any":
+    """Return field's value just before saving."""
+    value = super().pre_save(model_instance, add)
+    if value:
+      for bleached, restored in getattr(settings, self.CONFIG_KEY, {}).items():
+        value = value.replace(bleached, restored)
+      setattr(model_instance, self.attname, value)
     return value

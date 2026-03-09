f08f2d16d4482db712313283d0f7dc1e7222a17a
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-08 21:28:13 -0400

    feat(UTILITIES): transition to django_nh3

--- cross_shopper/utilities/models/serializers/fields/mixins/peroxide.py
+++ cross_shopper/utilities/models/serializers/fields/mixins/peroxide.py
@@ -2,23 +2,33 @@
 
 from typing import Optional
 
-from bleach import clean
+import nh3
 from django.conf import settings
-from django_bleach.utils import get_bleach_default_options
+from django_nh3.utils import get_nh3_configured_default_options
 
 
 class PeroxideFieldMixin:
   """A serializer mixin for sanitizing input values."""
 
-  CONFIG_KEY = "BLEACH_RESTORE_CONFIG"
+  CONFIG_KEY = "NH3_RESTORE_CONFIG"
 
   def sanitize(self, value: Optional[str]) -> str:
     """Appliy HTML sanitization to the input value and return a safe value."""
-    bleach_kwargs = get_bleach_default_options()
+    nh3_kwargs = get_nh3_configured_default_options()
 
-    clean_value = clean(value, **bleach_kwargs) if value else ""
+    clean_value = nh3.clean(value, **nh3_kwargs) if value else ""
 
-    for bleached, restored in getattr(settings, self.CONFIG_KEY, {}).items():
-      clean_value = clean_value.replace(bleached, restored)
+    # nh3 leaves an empty style="" if it cleans all style attributes.
+    # remove it manually until ammonia/nh3 addresses it
+    clean_value = clean_value.replace(
+        ' style=""',
+        "",
+    ).replace(
+        " style=''",
+        "",
+    )
+
+    for cleaned, restored in getattr(settings, self.CONFIG_KEY, {}).items():
+      clean_value = clean_value.replace(cleaned, restored)
 
     return clean_value

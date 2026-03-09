d24e643b3948cc0efc4761333a848d54d9bf50e7
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-09 09:16:04 -0400

    feat(UTILITIES): migration for title field format

---
+++ cross_shopper/scrapers/migrations/0005_refactored_title_field.py
@@ -0,0 +1,36 @@
+"""Scraper data migration for TitleField refactor."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, cast
+
+from django.db import migrations, transaction
+
+if TYPE_CHECKING:
+  from django.apps.registry import Apps
+  from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+  from scrapers.models import Scraper
+
+
+def save_all_func(
+    apps: Apps,
+    schema_editor: BaseDatabaseSchemaEditor,
+) -> None:
+  ScraperModel = cast(
+      "Scraper",
+      apps.get_model("scrapers", "Scraper"),
+  )
+
+  with transaction.atomic():
+    for scraper in ScraperModel.objects.all():
+      scraper.save()
+
+
+class Migration(migrations.Migration):
+
+  dependencies = [
+      ("scrapers", "0004_alter_scraperconfig_url_constraints"),
+  ]
+
+  operations = [
+      migrations.RunPython(save_all_func, save_all_func),
+  ]

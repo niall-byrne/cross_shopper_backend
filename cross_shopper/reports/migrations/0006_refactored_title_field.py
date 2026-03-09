cc2a28863229fef19b668b3bbdf09b63737158d4
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-09 09:16:04 -0400

    feat(UTILITIES): migration for title field format

---
+++ cross_shopper/reports/migrations/0006_refactored_title_field.py
@@ -0,0 +1,35 @@
+"""Report data migration for TitleField refactor."""
+
+from typing import TYPE_CHECKING, cast
+
+from django.db import migrations, transaction
+
+if TYPE_CHECKING:
+  from django.apps.registry import Apps
+  from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+  from reports.models import Report
+
+
+def save_all_func(
+    apps: "Apps",
+    schema_editor: "BaseDatabaseSchemaEditor",
+) -> None:
+  ReportModel = cast(
+      "Report",
+      apps.get_model('reports', 'Report'),
+  )
+
+  with transaction.atomic():
+    for scraper in ReportModel.objects.all():
+      scraper.save()
+
+
+class Migration(migrations.Migration):
+
+  dependencies = [
+      ('reports', '0005_alter_report_name_constraints'),
+  ]
+
+  operations = [
+      migrations.RunPython(save_all_func, save_all_func),
+  ]

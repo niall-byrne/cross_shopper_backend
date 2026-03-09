fb6edc591cd09d5e6519ee396a1b00832d9ca3d2
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-09 12:23:40 -0400

    feat(API): report targets endpoint

---
+++ cross_shopper/reports/models/serializers/read_only/report_target.py
@@ -0,0 +1,15 @@
+"""Serializer to retrieve or list Report model targets."""
+
+from reports.models import Report
+from rest_framework import serializers
+
+
+class ReportTargetSerializerRO(serializers.ModelSerializer[Report]):
+  """Serializer to retrieve or list Report model targets."""
+
+  class Meta:
+    model = Report
+    fields = (
+        'id',
+        'name',
+    )

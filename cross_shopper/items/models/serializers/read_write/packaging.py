928eff0cda0ca1f541df59c54a7bd9527d07b8c2
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-09 12:42:37 -0400

    refactor(UTILITIES): extract creatable slug related serializer field

--- cross_shopper/items/models/serializers/read_write/packaging.py
+++ cross_shopper/items/models/serializers/read_write/packaging.py
@@ -1,14 +1,20 @@
 """Serializer to retrieve, list, create or update Packaging."""
 
 from decimal import Decimal
-from typing import Any, Dict, Optional
+from typing import TYPE_CHECKING
 
 from items.models import Packaging, PackagingContainer, PackagingUnit
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from utilities.models.serializers.fields.blonde import BlondeCharField
-from utilities.models.serializers.fields.title import TitleField
-from .packaging_internal import PackagingSerializerInternal
+from rest_framework.validators import UniqueTogetherValidator
+from utilities.models.serializers.fields.slug_related_field import (
+    CreatableSlugRelatedField,
+)
+
+if TYPE_CHECKING:  # no cover
+  from typing import Any, Dict, List, Optional
+
+  from rest_framework.validators import Validator
 
 
 class PackagingSerializerRW(serializers.ModelSerializer[Packaging]):
@@ -27,51 +33,45 @@ class PackagingSerializerRW(serializers.ModelSerializer[Packaging]):
       max_digits=11,
       decimal_places=2,
   )
-  unit = BlondeCharField(max_length=80, allow_blank=False)
-  container = TitleField(max_length=80, allow_blank=False, allow_null=True)
+  unit = CreatableSlugRelatedField(
+      case_sensitive=False,
+      queryset=PackagingUnit.objects.all(),
+      slug_field="name",
+  )
+  container = CreatableSlugRelatedField(
+      allow_null=True,
+      case_sensitive=False,
+      queryset=PackagingContainer.objects.all(),
+      slug_field="name",
+  )
 
   class Meta:
     model = Packaging
     fields = ("quantity", "unit", "container")
     read_only = fields
 
-  def to_representation(self, instance: Packaging) -> Dict[str, str]:
-    """Object instance -> Dict of primitive datatypes."""
-    representation = PackagingSerializerInternal(instance).data
-
-    representation["unit"] = representation["unit"]["name"]
-
-    if representation["container"] is not None:
-      representation["container"] = representation["container"]["name"]
-
-    return representation
-
   def create(
       self,
-      validated_data: Dict[str, Any],
+      validated_data: "Dict[str, Any]",
   ) -> Packaging:
     """Create a new instance."""
-    container = PackagingContainer.objects.get_or_create(
-        name=validated_data.pop("container"),
-    )[0]
+    return Packaging.objects.get_or_create(**validated_data,)[0]
 
-    unit = PackagingUnit.objects.get_or_create(
-        name=validated_data.pop("unit"),
-    )[0]
+  def get_validators(self) -> "List[Validator[Any]]":
+    """Filter out the UniqueTogetherValidator."""
+    validators = super().get_validators()
+    return [
+        validator for validator in validators
+        if not isinstance(validator, UniqueTogetherValidator)
+    ]
 
-    return Packaging.objects.get_or_create(
-        **validated_data,
-        container=container,
-        unit=unit,
-    )[0]
-
-  def validate_quantity(self, value: Optional[int]) -> Optional[int]:
+  def validate_quantity(self, value: "Optional[int]") -> "Optional[int]":
     """Perform quantity field validation."""
     if value is None and self.initial_data["container"] is not None:
       raise ValidationError(self.ERROR_MESSAGE_QUANTITY_IS_NULL)
     return value
 
-  def validate_container(self, value: Optional[str]) -> Optional[str]:
+  def validate_container(self, value: "Optional[str]") -> "Optional[str]":
     """Perform container field validation."""
     if value is None and self.initial_data["quantity"] is not None:
       raise ValidationError(self.ERROR_MESSAGE_CONTAINER_IS_NULL)

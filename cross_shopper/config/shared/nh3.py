b5a286d9610271c04e3eab5670e71451b5ca0103
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-08 21:28:13 -0400

    feat(UTILITIES): transition to django_nh3

---
+++ cross_shopper/config/shared/nh3.py
@@ -0,0 +1,24 @@
+"""Configuration for django-nh3."""
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:  # no cover
+  from typing import Callable, Dict, Optional, Set
+
+  AliasNh3AllowAttributesFilter = Optional[Callable[[str, str, str],
+                                                    Optional[str]]]
+
+# Which HTML values to restore after transformation
+NH3_RESTORE_CONFIG: "Dict[str, str]" = {"&amp;": "&"}
+
+NH3_ALLOWED_ATTRIBUTES: "Dict[str, str]" = {}
+
+NH3_ALLOWED_ATTRIBUTES_FILTER: "AliasNh3AllowAttributesFilter" = None
+
+NH3_CLEAN_CONTENT_TAGS: "Set[str]" = set()
+
+NH3_LINK_REL: "str" = ""
+
+NH3_STRIP_COMMENTS: "bool" = False
+
+NH3_ALLOWED_TAGS: "Set[str]" = set()

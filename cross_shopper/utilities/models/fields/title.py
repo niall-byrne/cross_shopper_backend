39f90c3db557088e0abdb5440e9b33408faca301
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-08 20:36:35 -0400

    refactor(UTILITIES): separate transform and bleach field bases

--- cross_shopper/utilities/models/fields/title.py
+++ cross_shopper/utilities/models/fields/title.py
@@ -2,15 +2,19 @@
 
 from typing import TypeVar
 
+from utilities.models.fields.bases.transform_base import TransformCharFieldBase
 from .blonde import BlondeCharField
 
 _ST = TypeVar("_ST")
 _GT = TypeVar("_GT")
 
 
-class TitleField(BlondeCharField[_ST, _GT]):
+class TitleField(
+    TransformCharFieldBase[_ST, _GT],
+    BlondeCharField[_ST, _GT],
+):
+  """Adds title case transformations to the BlondeCharField."""
 
   def transform(self, value: str) -> str:
     """Transform the field value into a new form."""
-    value = super().transform(value)
     return value.capitalize()

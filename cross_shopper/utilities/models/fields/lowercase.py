b9a7244a8238555df74aede901e4520b60e2f015
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-08 20:36:35 -0400

    refactor(UTILITIES): separate transform and bleach field bases

--- cross_shopper/utilities/models/fields/lowercase.py
+++ cross_shopper/utilities/models/fields/lowercase.py
@@ -2,16 +2,19 @@
 
 from typing import TypeVar
 
+from utilities.models.fields.bases.transform_base import TransformCharFieldBase
 from .blonde import BlondeCharField
 
 _ST = TypeVar("_ST")
 _GT = TypeVar("_GT")
 
 
-class LowerCaseField(BlondeCharField[_ST, _GT]):
+class LowerCaseField(
+    TransformCharFieldBase[_ST, _GT],
+    BlondeCharField[_ST, _GT],
+):
   """Adds lower case transformations to the BlondeCharField."""
 
-  def transform(self, value: str) -> str:
+  def transform(self, value: "str") -> "str":
     """Transform the field value into a new form."""
-    value = super().transform(value)
     return value.lower()

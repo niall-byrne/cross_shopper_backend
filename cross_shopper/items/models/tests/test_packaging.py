353386bb425e141be00d833f8b84f20b3d54176e
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-12 12:56:15 -0400

    refactor(CONSTANTS): add modules for model constants

--- cross_shopper/items/models/tests/test_packaging.py
+++ cross_shopper/items/models/tests/test_packaging.py
@@ -2,6 +2,7 @@
 
 import pytest
 from django.core.exceptions import ValidationError
+from items import constants
 from items.models import Packaging, PackagingContainer, PackagingUnit
 
 
@@ -126,6 +127,6 @@ class TestPackaging:
     )
 
     assert str(packaging) == (
-        f"{packaging.CONTAINER_NAME_FOR_BULK_PACKAGING} per "
+        f"{constants.PACKAGING_CONTAINER_NAME_FOR_BULK_PACKAGING} per "
         f"{str(packaging.unit)}"
     )

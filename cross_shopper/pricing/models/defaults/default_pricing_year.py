433e54e2ab6d00266631037a2f02e9fd81037885
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-12 12:56:15 -0400

    refactor(CONSTANTS): add modules for model constants

--- cross_shopper/pricing/models/defaults/default_pricing_year.py
+++ cross_shopper/pricing/models/defaults/default_pricing_year.py
@@ -2,17 +2,14 @@
 
 from datetime import timedelta
 
-from django.apps import apps
 from django.utils import timezone
+from pricing.constants import DAY_OF_WEEK_FOR_PRICING
 
 
 def default_pricing_year() -> int:
   """Generate the current year."""
-  day_of_week_for_pricing = apps.get_model(
-      'pricing.Price'
-  ).DAY_OF_WEEK_FOR_PRICING
   today = timezone.now()
-  if today.weekday() >= day_of_week_for_pricing:
+  if today.weekday() >= DAY_OF_WEEK_FOR_PRICING:
     if (today + timedelta(weeks=1)).year > today.year:
       return today.year + 1
   return today.year

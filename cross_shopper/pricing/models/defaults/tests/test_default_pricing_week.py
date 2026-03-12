ada1ead6861e4b821cb3866ac0ffc40f58b61a3c
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-03-12 12:56:15 -0400

    refactor(CONSTANTS): add modules for model constants

--- cross_shopper/pricing/models/defaults/tests/test_default_pricing_week.py
+++ cross_shopper/pricing/models/defaults/tests/test_default_pricing_week.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 
 import pytest
 from freezegun import freeze_time
-from pricing.models import Price
+from pricing.constants import DAY_OF_WEEK_FOR_PRICING
 from pricing.models.defaults import default_pricing_week
 from .scenarios import eoy_boundary_scenarios
 
@@ -14,7 +14,7 @@ class TestDefaultPricingWeek:
 
   @pytest.mark.parametrize(
       "weekday",
-      list(range(0, Price.DAY_OF_WEEK_FOR_PRICING)),
+      list(range(0, DAY_OF_WEEK_FOR_PRICING)),
   )
   def test__call__before_prices_change__returns_expected_value(
       self,
@@ -27,7 +27,7 @@ class TestDefaultPricingWeek:
 
   @pytest.mark.parametrize(
       "weekday",
-      list(range(Price.DAY_OF_WEEK_FOR_PRICING, 7)),
+      list(range(DAY_OF_WEEK_FOR_PRICING, 7)),
   )
   def test__call__after_prices_change__returns_expected_value(
       self,
